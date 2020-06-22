3693c000eff0021f6c98e2aad5a0e195dfa2059c
Alejandro Berrio <alejo.berrio@duke.edu>
2020-06-22 13:52:54 -0400

    Update bfgenerator_global.py

--- bfgenerator_global.py
+++ bfgenerator_global.py
@@ -27,7 +27,7 @@ for lrt in model:
 			f.write('tree= "((((hg19,panTro4),gorGor3),ponAbe2),rheMac3)";\n')
 			f.write('fgrnd_branch_name = "%s";\n' % ape)
 			f.write('res_file = "res/%s.%s.%s.res";\n' % (i,ape,lrt))
-			f.write('#include "%s3-fgrnd_spec.bf";\n' % lrt)
+			f.write('#include "%s4-fgrnd_spec.bf";\n' % lrt)
 
 
 
