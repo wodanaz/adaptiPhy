97a4be98aab4839bdda830ba33fc31d6462aaacf
Alejandro Berrio <alejo.berrio@duke.edu>
2025-12-03 15:22:46 -0500

    Modify SLURM script for email and account settings
    Updated email and account information in SLURM script.

---
+++ neutral_smk/slurm-launch-neutral-smk.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH --mail-type=END
+#SBATCH --mail-user=youremail@university.edu
+#SBATCH -N 1
+#SBATCH --account=yourlab
+#SBATCH --partition=common
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=12G # modify if you need less or more
+#SBATCH -J build_neutral_proxy
+set -euo pipefail
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate snakemake
+snakemake \
+  --profile slurm_general \
+  --use-conda \
+  --conda-prefix /hpc/group/wraylab/ab620/conda \
+  --keep-going
